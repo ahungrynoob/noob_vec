98ec3f6fc23ead7e8526d5194d25123c795cbcbb
ahungrynoob <dxd_sjtu@outlook.com>
2021-08-15 22:09:38 +0800

    feat: myvec::grow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(ptr_internals)]
-use std::mem;
-use std::ptr::{self, Unique}; // 定义一个 unique类型，满足如下条件： 为了锁定内存的内容和owner, 让裸指针拥有数据
-                              // 对T可变;
-                              // 拥有类型T的值
-                              // 如果T是Send/Sync，那么Unique也是Send/Sync
-                              // 指针永远不为空
+use std::ptr::{self, Unique};
+use std::{mem, panic}; // 定义一个 unique类型，满足如下条件： 为了锁定内存的内容和owner, 让裸指针拥有数据
+                       // 对T可变;
+                       // 拥有类型T的值
+                       // 如果T是Send/Sync，那么Unique也是Send/Sync
+                       // 指针永远不为空
+use std::alloc::{alloc, handle_alloc_error, realloc, Layout};
 struct MyVec<T> {
     ptr: Unique<T>,
     cap: usize,
@@ -18,6 +19,44 @@ impl<T> MyVec<T> {
             ptr: Unique::dangling(),
             cap: 0,
             len: 0,
+        }
+    }
+
+    fn grow(&mut self) {
+        unsafe {
+            // 获取到T类型的对齐方式
+            let align = mem::align_of::<T>();
+            let elem_size = mem::size_of::<T>();
+            let layout: Layout;
+
+            let (new_cap, ptr) = if self.cap == 0 {
+                layout = Layout::from_size_align_unchecked(elem_size, align);
+                let ptr = alloc(layout);
+                (1, ptr)
+            } else {
+                let new_cap = self.cap * 2;
+                let old_num_bytes = self.cap * elem_size;
+                assert!(
+                    old_num_bytes <= (isize::MAX as usize) / 2,
+                    "capacity overflow"
+                );
+                let new_num_bytes = old_num_bytes * 2;
+                layout = Layout::from_size_align_unchecked(new_num_bytes, align);
+                let ptr = realloc(self.ptr.as_ptr() as *mut _, layout, new_num_bytes);
+                (new_cap, ptr)
+            };
+
+            if ptr.is_null() {
+                handle_alloc_error(layout)
+            }
+
+            if let Some(ptr) = Unique::new(ptr as *mut _) {
+                self.ptr = ptr;
+            } else {
+                panic!("error");
+            }
+
+            self.cap = new_cap;
         }
     }
 }
